0fa217be7c7f15296a1cdcf25fbbb59c1a2bd875
SDuff01 <39769612+SDuff01@users.noreply.github.com>
2021-07-08 10:31:25 +0100

    Update RunTimezoneArtifact.ps1

--- RunTimezoneArtifact.ps1
+++ RunTimezoneArtifact.ps1
@@ -1,3 +1,6 @@
-Set-WinSystemLocale -SystemLocale en-GB
-Set-TimeZone -Name "GMT Standard Time"
-Set-WinUserLanguageList -Language en-GB -Force
+$svc_name = 'Network List Service'
+Set-Service -StartupType Disabled $svc_name
+$svc_name2 = 'Network Location Awareness'
+Set-Service -StartupType Disabled $svc_name2
+$svc_name3 = 'Error Reporting Service'
+Set-Service -StartupType Disabled $svc_name3
