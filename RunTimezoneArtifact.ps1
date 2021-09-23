2a335f6aeb36ab8bd5c133b07075e43e959511e9
SDuff01 <39769612+SDuff01@users.noreply.github.com>
2021-09-23 11:39:50 +0100

    Update RunTimezoneArtifact.ps1

--- RunTimezoneArtifact.ps1
+++ RunTimezoneArtifact.ps1
@@ -1,6 +1,68 @@
-$svc_name = 'Network List Service'
-Set-Service -StartupType Disabled $svc_name
-$svc_name2 = 'Network Location Awareness'
-Set-Service -StartupType Disabled $svc_name2
-$svc_name3 = 'Error Reporting Service'
-Set-Service -StartupType Disabled $svc_name3
+#$svc_name = 'Network List Service'
+#Set-Service -StartupType Disabled $svc_name
+#$svc_name2 = 'Network Location Awareness'
+#Set-Service -StartupType Disabled $svc_name2
+#$svc_name3 = 'Error Reporting Service'
+#Set-Service -StartupType Disabled $svc_name3
+###################################################################################################
+
+#
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Ensure we set the working directory to that of the script.
+pushd $PSScriptRoot
+
+###################################################################################################
+
+#
+# Functions used in this script.
+#
+$scriptFolder = "c:\\buildArtifacts\\Artifacts\\TimeZone"
+$functionFiles = Get-ChildItem -Path $scriptFolder\* -Include "*artifact-funcs*.ps1" -Exclude "*.tests.ps1" 
+foreach($file in $functionFiles)
+{
+    $fileName = $file.Name
+    ."./$fileName"
+}
+
+###################################################################################################
+
+#
+# Handle all errors in this script.
+#
+
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    Handle-LastError
+}
+
+###################################################################################################
+
+#
+# Main execution block.
+#
+
+try
+{
+    #arguments passed to this script should be passed to the artifact script
+    $command = $Script:MyInvocation.MyCommand
+    $scriptName = $Script:MyInvocation.MyCommand.Name
+    $scriptLine = $MyInvocation.Line
+    
+    $region = "GB"
+
+    iex ".\artifact.ps1 $region"
+
+    Write-Host 'Artifact installed successfully.'
+}
+finally
+{
+    popd
+}
